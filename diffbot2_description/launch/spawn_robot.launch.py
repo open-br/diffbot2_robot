87000f98f0cfa1486d2d51db56683b911e4e64e2
Mateus Amarante Araújo <mateus.amarujo@gmail.com>
2020-10-31 18:43:21 -0300

    Fix bad quotes

--- diffbot2_description/launch/spawn_robot.launch.py
+++ diffbot2_description/launch/spawn_robot.launch.py
@@ -2,18 +2,17 @@
 #
 # Copyright 2020 Open BR
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
+# Licensed under the Apache License, Version 2.0 (the 'License');
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
 #     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
+# distributed under the License is distributed on an 'AS IS' BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 
 import os
 
@@ -26,10 +25,10 @@ import xacro
 
 def generate_launch_description():
     # Get dir names
-    pkg_share = get_package_share_directory("diffbot2_description")
+    pkg_share = get_package_share_directory('diffbot2_description')
 
     # Compute robot_description string
-    xacro_file = os.path.join(pkg_share, "urdf/diffbot2.xacro")
+    xacro_file = os.path.join(pkg_share, 'urdf/diffbot2.xacro')
     robot_description = xacro.process(xacro_file)
 
     # Launch description
@@ -37,25 +36,25 @@ def generate_launch_description():
 
     # Define parameters
     common_params = {
-        "robot_description": robot_description,
+        'robot_description': robot_description,
     }
 
     # Add nodes
     launch_description.add_action(
         Node(
-            package="robot_state_publisher",
-            executable="robot_state_publisher",
-            name="robot_state_publisher",
-            output="screen",
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
             parameters=[common_params],
         )
     )
     launch_description.add_action(
         Node(
-            package="joint_state_publisher",
-            executable="joint_state_publisher",
-            name="joint_state_publisher",
-            output="screen",
+            package='joint_state_publisher',
+            executable='joint_state_publisher',
+            name='joint_state_publisher',
+            output='screen',
             parameters=[common_params],
         ),
     )
