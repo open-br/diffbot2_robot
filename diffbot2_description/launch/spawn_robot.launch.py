8d5465add4fcc626a01feab04ada0c88f8f7ef4a
Mateus Amarante Araújo <mateus.amarujo@gmail.com>
2020-10-31 09:44:41 -0300

    Update format with black formatter

--- diffbot2_description/launch/spawn_robot.launch.py
+++ diffbot2_description/launch/spawn_robot.launch.py
@@ -23,22 +23,25 @@ import xacro
 
 def generate_launch_description():
     pkg_share = ament_index_python.get_package_share_directory(
-        'diffbot2_description')
-    xacro_file = pkg_share + '/urdf/diffbot2.xacro'
+        "diffbot2_description", "alo", "alow"
+    )
+    xacro_file = pkg_share + "/urdf/diffbot2.xacro"
     robot_description = xacro.process(xacro_file)
-    return launch.LaunchDescription([
-        launch_ros.actions.Node(
-            package='robot_state_publisher',
-            executable='robot_state_publisher',
-            name='robot_state_publisher',
-            output='screen',
-            parameters=[{'robot_description': robot_description}]
-        ),
-        launch_ros.actions.Node(
-            package='joint_state_publisher',
-            executable='joint_state_publisher',
-            name='joint_state_publisher',
-            output='screen',
-            parameters=[{'robot_description': robot_description}]
-        )
-    ])
+    return launch.LaunchDescription(
+        [
+            launch_ros.actions.Node(
+                package="robot_state_publisher",
+                executable="robot_state_publisher",
+                name="robot_state_publisher",
+                output="screen",
+                parameters=[{"robot_description": robot_description}],
+            ),
+            launch_ros.actions.Node(
+                package="joint_state_publisher",
+                executable="joint_state_publisher",
+                name="joint_state_publisher",
+                output="screen",
+                parameters=[{"robot_description": robot_description}],
+            ),
+        ]
+    )
